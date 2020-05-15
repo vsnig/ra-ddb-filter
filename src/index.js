e1af7807741b7ab49b89744e8d0bd4fb2416b3d5
Viacheslav <4790093+snegostup@users.noreply.github.com>
2020-05-15 14:10:08 +0300

    code

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const R = require('ramda')
+
+const makeFilterVals = (filter) => {
+  const makeFilterDdb = (field, vals) => {
+    vals = vals.map((x) => x[0])
+    const expression = '(' + vals.map((_, i) => `#${field} = :${field}${i}`).join(' OR ') + ')'
+    const attrNames = { [`#${field}`]: field }
+    const attrVals = vals.reduce((acc, curr, i) => {
+      acc[`:${field}${i}`] = curr
+      return acc
+    }, {})
+
+    return { expression, attrNames, attrVals }
+  }
+
+  const makeFilter = ([key, val]) => {
+    const field = key.slice(0, -1)
+    return makeFilterDdb(field, val)
+  }
+
+  const mergeFilterVals = (prev, curr) => ({
+    expression: [prev.expression, curr.expression].join(' AND '),
+    attrNames: { ...prev.attrNames, ...curr.attrNames },
+    attrVals: { ...prev.attrVals, ...curr.attrVals },
+  })
+
+  const initVals = { expression: '', attrNames: {}, attrVals: {} }
+  if (!R.isEmpty(filter)) {
+    return R.pipe(R.toPairs, R.map(makeFilter), R.reduce(mergeFilterVals, initVals))(filter)
+  }
+  return initVals
+}
+
+module.exports = {
+  makeFilterVals,
+}
